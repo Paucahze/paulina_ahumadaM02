d9fad73334a03790019addf7bb5106ce45fcdf60
Paucahze <paulina.ahumada.z@gmail.com>
2024-08-30 13:02:54 -0400

    ajustes en js,nav y se agrego modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,52 +13,15 @@ function mostrarOcultarImagenes() {
     }
 }
 
-//---------------
 
-
-/*//Al pasar el mouse por el primer <div> “text1” se mostrará el segundo y al salir de él se ocultará nuevamente. 
-function ocultarMensaje() {
-    document.getElementById("text2").style.display = "none";
-}
-function mostrarMensaje() {
-    document.getElementById("text2").style.display = "block";
+//Flashes 
+function mostrarOcultarImagen(element) {
+    element.style.display = 'none';
+    element.nextElementSibling.style.display = 'block';
 }
 
-// Al hacer clic en el <div> id “caja2” la imagen deberá agrandarse un 100% y al salir de ella, volver a su tamaño inicial. 
-function agrandarImg() {
-    document.getElementById("img").style.width = "100%";
+function restaurarImagen(element) {
+    element.style.display = 'block';
+    element.nextElementSibling.style.display = 'none';
 }
-function retornarImg() {
-    document.getElementById("img").style.width = "20%";
-}
-
-//Con el tercer <div> la letra se agrandará al hacer doble clic en él. ONDBLCLICK */
-function agrandar() {
-    document.getElementById("caja3").style.fontSize = "2em";
-}
-function achicar() {
-    document.getElementById("caja3").style.fontSize = "1em";
-}
-
-//-----
-
-
-
-
-document.querySelectorAll('.imagen-contenedor').forEach(contenedor => {
-    const imagenVisible = contenedor.querySelector('img:first-child');
-    const imagenOculta = contenedor.querySelector('.imagenOculta');
-
-    contenedor.addEventListener('mouseover', () => {
-        imagenVisible.style.display = 'none';
-        imagenOculta.style.display = 'block';
-    });
-
-    contenedor.addEventListener('mouseout', () => {
-        imagenVisible.style.display = 'block';
-        imagenOculta.style.display = 'none';
-    });
-});
-
-//_______
 
