fd604879d5217cae1a1796b4bdf52e1ea8c756b0
Paucahze <paulina.ahumada.z@gmail.com>
2024-08-30 11:46:11 -0400

    inicio imagen firma  e interaccion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,49 @@
+//Inicio imagen firma
+
+function mostrarOcultarImagenes() {
+    const imagenUno = document.getElementById("imagen-firma-uno");
+    const imagenDos = document.getElementById("imagen-firma-dos");
+
+    if (imagenUno.style.display !== "none") {
+        imagenUno.style.display = "none";
+        imagenDos.style.display = "block";
+    } else {
+        imagenUno.style.display = "block";
+        imagenDos.style.display = "none";
+    }
+}
+
+//---------------
+
+
+/*//Al pasar el mouse por el primer <div> “text1” se mostrará el segundo y al salir de él se ocultará nuevamente. 
+function ocultarMensaje() {
+    document.getElementById("text2").style.display = "none";
+}
+function mostrarMensaje() {
+    document.getElementById("text2").style.display = "block";
+}
+
+// Al hacer clic en el <div> id “caja2” la imagen deberá agrandarse un 100% y al salir de ella, volver a su tamaño inicial. 
+function agrandarImg() {
+    document.getElementById("img").style.width = "100%";
+}
+function retornarImg() {
+    document.getElementById("img").style.width = "20%";
+}
+
+//Con el tercer <div> la letra se agrandará al hacer doble clic en él. ONDBLCLICK */
+function agrandar() {
+    document.getElementById("caja3").style.fontSize = "2em";
+}
+function achicar() {
+    document.getElementById("caja3").style.fontSize = "1em";
+}
+
+//-----
+
+
+
 
 document.querySelectorAll('.imagen-contenedor').forEach(contenedor => {
     const imagenVisible = contenedor.querySelector('img:first-child');
@@ -16,9 +62,3 @@ document.querySelectorAll('.imagen-contenedor').forEach(contenedor => {
 
 //_______
 
-
-document.getElementById("imagen").addEventListener("dblclick", function () {
-    this.src = "assets/img/SwamiAgria/tarjeta_pequeña.jpg";
-    this.style.width = "200px";
-    document.getElementById("mensaje").style.display = "block";
-});
